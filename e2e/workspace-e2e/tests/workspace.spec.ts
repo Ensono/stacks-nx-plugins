96980ddaa45f5b5d95a082234f61ff1d856067e5
Gareth Cozens <gareth.cozens@amido.com>
2022-11-27 17:22:28 +0000

    chore(root): add github workflows

---
+++ e2e/workspace-e2e/tests/workspace.spec.ts
@@ -0,0 +1,30 @@
+import {
+  checkFilesExist,
+  ensureNxProject,
+  readJson,
+  runNxCommandAsync,
+  uniq,
+} from '@nrwl/nx-plugin/testing';
+
+describe('workspace e2e', () => {
+  // Setting up individual workspaces per
+  // test can cause e2e runs to take a long time.
+  // For this reason, we recommend each suite only
+  // consumes 1 workspace. The tests should each operate
+  // on a unique project in the workspace, such that they
+  // are not dependant on one another.
+  beforeAll(() => {
+    ensureNxProject('@ensono-stacks/workspace', 'dist/packages/workspace');
+  });
+
+  afterAll(() => {
+    // `nx reset` kills the daemon, and performs
+    // some work which can help clean up e2e leftovers
+    runNxCommandAsync('reset');
+  });
+
+  // TODO: add relevant test
+  it('test', () => {
+    expect(true).toBe(true);
+  });
+});
