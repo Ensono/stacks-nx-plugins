3142e50498bba21314d49d9c8026444284a6a228
Nandor Szentpeteri <nandor.szentpeteri@amido.com>
2022-12-06 10:50:15 +0000

    feat(rest-client): use nrwl js library generator in http-client

--- packages/rest-client/src/generators/http-client/generator.spec.ts
+++ packages/rest-client/src/generators/http-client/generator.spec.ts
@@ -1,20 +1,36 @@
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
-import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+import { Tree, readProjectConfiguration, readJson } from '@nrwl/devkit';
 
 import generator from './generator';
 import { HttpClientGeneratorSchema } from './schema';
 
-describe('rest-client generator', () => {
-  let appTree: Tree;
-  const options: HttpClientGeneratorSchema = { name: 'test', unitTestRunner: 'jest', linter: 'eslint' };
+describe('http-client generator', () => {
+  let tree: Tree;
+  const options: HttpClientGeneratorSchema = { name: 'testClient' };
 
   beforeEach(() => {
-    appTree = createTreeWithEmptyWorkspace();
+    tree = createTreeWithEmptyWorkspace();
   });
 
-  it('should run successfully', async () => {
-    await generator(appTree, options);
-    const config = readProjectConfiguration(appTree, 'test');
+  it('should generate the http-client', async () => {
+    await generator(tree, {
+      ...options,
+      directory: 'custom',
+      tags: 'test, client',
+    });
+
+    const config = readProjectConfiguration(tree, 'custom-test-client');
+
     expect(config).toBeDefined();
+    expect(config.tags).toEqual(['test', 'client']);
+  });
+
+  it('should install axios as dependency', async () => {
+    await generator(tree, options);
+
+    const packageJson = readJson(tree, 'package.json');
+    expect(Object.keys(packageJson.dependencies)).toEqual(
+      expect.arrayContaining(['axios'])
+    );
   });
 });
--- packages/rest-client/src/generators/http-client/schema.d.ts
+++ packages/rest-client/src/generators/http-client/schema.d.ts
@@ -1,7 +1,5 @@
 export interface HttpClientGeneratorSchema {
   name: string;
-  linter: Linter;
-  unitTestRunner: 'jest' | 'none';
   directory?: string;
   tags?: string;
   skipFormat?: boolean;
