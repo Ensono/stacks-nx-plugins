c6c9e59caf52a3ecb36b552a45742d7f2efabdc5
Nandor Szentpeteri <nandor.szentpeteri@amido.com>
2022-12-14 17:48:35 +0000

    fix(rest-client): remove __dirname to search for files in the project root, fix unit tests

--- packages/rest-client/src/generators/bump-version/generator.spec.ts
+++ packages/rest-client/src/generators/bump-version/generator.spec.ts
@@ -1,8 +1,14 @@
-import { Tree } from '@nrwl/devkit';
+import { Tree, generateFiles } from '@nrwl/devkit';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 
 import generator from './generator';
 import { BumpVersionGeneratorSchema } from './schema';
+
+jest.mock('@nrwl/devkit', () => ({
+    __esModule: true,
+    ...jest.requireActual('@nrwl/devkit'),
+    generateFiles: jest.fn(),
+}));
 
 describe('bump-version generator', () => {
     let tree: Tree;
@@ -21,12 +27,20 @@ describe('bump-version generator', () => {
         tree.write('fixtures/endpoints/test/V1/index.ts', 'test');
         await generator(tree, {
             ...options,
-            endpointPath: './fixtures/endpoints',
+            endpointPath: 'fixtures/endpoints',
         });
 
-        expect(
-            tree.exists('./fixtures/endpoints/test/V2/index.ts'),
-        ).toBeTruthy();
+        expect(generateFiles).toHaveBeenCalledWith(
+            tree,
+            'fixtures/endpoints/test/V1',
+            'fixtures/endpoints/test/V2',
+            {
+                endpoint: 'test',
+                endpointPath: 'fixtures/endpoints',
+                endpointVersion: 2,
+                template: '',
+            },
+        );
     });
 
     it("should throw an error if the endpoint doesn't exist", async () => {
