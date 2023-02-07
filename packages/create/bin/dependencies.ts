50fecb400a19b7cfb8524939473cf89f2e328420
Cerden Mincher <cerdenmincher@amido.com>
2023-02-07 12:10:28 +0000

    feat(create): use apps preset for Next (WIP)

--- packages/create/bin/dependencies.ts
+++ packages/create/bin/dependencies.ts
@@ -1,6 +1,6 @@
 import yargs from 'yargs';
 
-import { execAsync } from './exec';
+import { execAsync, execSync2 } from './exec';
 import {
     detectPackageManager,
     getPackageManagerCommand,
@@ -9,24 +9,38 @@ import { CreateStacksArguments, Preset } from './types';
 
 const stacksRequiredPlugins = ['@ensono-stacks/workspace'];
 
+export function verifyPreset(
+  args: string[]
+) {
+    console.log('This has happened')
+    console.log('Wheeeee')
+    console.log('args: ', args)
+    return args.map(arg => arg === 'next' ? 'apps' : arg)
+    // return args;
+}
+
 export function getGeneratorsToRun(
     argv: yargs.Arguments<CreateStacksArguments>,
 ) {
+    console.log('appname', argv.appName)
     const generators: string[] = [];
-    generators.push(`@ensono-stacks/workspace:init`);
+    generators.push(`@ensono-stacks/workspace:init --verbose`);
 
     if (argv.preset === Preset.NextJs) {
-        generators.push(`@ensono-stacks/next:init --project=${argv.appName}`);
+        // add @nrwl/next generator
+        generators.push(`@nrwl/next:app ${argv.appName} --e2eProvider=none --verbose`);
+        generators.push(`@ensono-stacks/next:init --project=${argv.appName} --verbose`);
     }
 
     return generators;
 }
 
+// TODO: Rename this function if we install nrwl packages as well?
 export function getStacksPlugins(argv: yargs.Arguments<CreateStacksArguments>) {
     const plugins = [...stacksRequiredPlugins];
 
     if (argv.preset === Preset.NextJs) {
-        plugins.push('@ensono-stacks/next');
+        plugins.push('@nrwl/next', '@ensono-stacks/next');
     }
 
     return plugins;
@@ -56,4 +70,19 @@ export async function runGenerators(commands: string[], cwd: string) {
     );
 
     return Promise.allSettled(promises);
+}
+
+export function runGeneratorsSync(commands: string[], cwd: string) {
+    if (commands.length === 0) {
+        return
+    }
+
+    const packageManager = detectPackageManager(cwd);
+    const pm = getPackageManagerCommand(packageManager);
+
+    commands.forEach(command =>
+      execSync2(`${pm.exec} nx g ${command}`, cwd)
+    );
+
+    // return Promise.allSettled(promises);
 }
