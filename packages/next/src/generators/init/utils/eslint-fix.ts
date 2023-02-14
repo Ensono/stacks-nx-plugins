5c2e4cf9ad5ba98e1ca00893495b8de120b6d50b
Michal Palys-Dudek <michal.palys-dudek@ensono.com>
2023-02-14 12:55:28 +0100

    fix(workspace): disable eslint-disable rule for all jest.config.ts files globally as they come from nrwl generator everywhere

--- packages/next/src/generators/init/utils/eslint-fix.ts
+++ packages/next/src/generators/init/utils/eslint-fix.ts
@@ -8,16 +8,6 @@ import { SyntaxKind } from 'ts-morph';
 export function eslintFix(project: ProjectConfiguration, tree: Tree) {
     const morphTree = tsMorphTree(tree);
 
-    // Specify the rules you want to disable  unicorn/no-abusive-eslint-disable
-    const jestConfigNode = morphTree.addSourceFileAtPath(
-        joinPathFragments(project.root, 'jest.config.ts'),
-    );
-    jestConfigNode
-        .getStatementsWithComments()
-        .find(node => node.getText() === '/* eslint-disable */')
-        ?.remove();
-    jestConfigNode.saveSync();
-
     // Using exported name 'Index' as identifier for default export  import/no-named-as-default
     const indexSpecNode = morphTree.addSourceFileAtPath(
         joinPathFragments(project.root, 'specs', 'index.spec.tsx'),
