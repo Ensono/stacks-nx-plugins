08e671599d06ac7b0db6e2c0e9912894c268b182
Nandor Szentpeteri <nandor.szentpeteri@amido.com>
2023-02-15 14:51:53 +0000

    feat(playwright): add pipeline code to playwright plugin

---
+++ packages/playwright/src/generators/init/utils/update-tasks-yamls.ts
@@ -0,0 +1,72 @@
+import { Tree } from '@nrwl/devkit';
+import YAML from 'yaml';
+
+export function updateTasksYaml(
+    tree: Tree,
+    { visualRegression }: { visualRegression: boolean },
+) {
+    if (!tree.exists('build/tasks.yaml')) {
+        throw new Error('tasks.yaml is missing from the build folder');
+    }
+
+    const tasks = YAML.parse(tree.read('build/tasks.yaml', 'utf-8'));
+    if (tasks.tasks) {
+        // Add e2e tasks
+        tasks.tasks = visualRegression
+            ? {
+                  ...tasks.tasks,
+                  'e2e:local': {
+                      description: 'Run e2e tests locally',
+                      command: [
+                          'npx nx affected --base="$BASE_SHA" --target=e2e-docker --parallel=1',
+                      ],
+                  },
+                  'e2e:ci': {
+                      description: 'Run e2e tests in ci',
+                      command: [
+                          'npx nx affected --base="$BASE_SHA" --target=e2e --parallel=1',
+                      ],
+                  },
+              }
+            : {
+                  ...tasks.tasks,
+                  e2e: {
+                      description: 'Run e2e tests',
+                      command: [
+                          'npx nx affected --base="$BASE_SHA" --target=e2e --parallel=1',
+                      ],
+                  },
+              };
+    }
+
+    tree.write('build/tasks.yaml', YAML.stringify(tasks));
+}
+
+export function updateTaskctlYaml(
+    tree: Tree,
+    { visualRegression }: { visualRegression: boolean },
+) {
+    if (!tree.exists('taskctl.yaml')) {
+        throw new Error('taskctl.yaml is missing from root');
+    }
+
+    const taskctl = YAML.parse(tree.read('taskctl.yaml', 'utf8'));
+    if (taskctl.pipelines) {
+        if (visualRegression) {
+            // Add local tasks
+            taskctl.pipelines.dev.push({ task: 'e2e:local' });
+            taskctl.pipelines.fe.push({ task: 'e2e:local' });
+            // Add ci tasks
+            taskctl.pipelines.nonprod.push({ task: 'e2e:ci' });
+            taskctl.pipelines.prod.push({ task: 'e2e:ci' });
+        } else {
+            // Add tasks
+            taskctl.pipelines.dev.push({ task: 'e2e' });
+            taskctl.pipelines.fe.push({ task: 'e2e' });
+            taskctl.pipelines.nonprod.push({ task: 'e2e' });
+            taskctl.pipelines.prod.push({ task: 'e2e' });
+        }
+    }
+
+    tree.write('taskctl.yaml', YAML.stringify(taskctl));
+}
