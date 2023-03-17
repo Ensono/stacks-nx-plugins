5e3262d92389f7c3e15ec8a5f9f006153af3cb8e
Nandor Szentpeteri <nandor.szentpeteri@amido.com>
2023-03-17 04:04:24 +0000

    fix(common-core): don't throw error in deploymentMessage function when stacks config is missing

--- packages/common/core/src/utils/deploymentGeneratorMessage.ts
+++ packages/common/core/src/utils/deploymentGeneratorMessage.ts
@@ -4,14 +4,18 @@ import chalk from 'chalk';
 import { readStacksConfig } from '../lib/stacks';
 
 export function deploymentGeneratorMessage(tree: Tree, message: string) {
-    const stacksConfig = readStacksConfig(tree);
+    try {
+        const stacksConfig = readStacksConfig(tree);
 
-    if (stacksConfig) {
-        console.log(
-            '\n',
-            chalk.yellow`If you would like to install the relevant deployment configuration, run the following generator: `,
-            chalk.magenta`${message}`,
-            '\n',
-        );
+        if (stacksConfig) {
+            console.log(
+                '\n',
+                chalk.yellow`If you would like to install the relevant deployment configuration, run the following generator: `,
+                chalk.magenta`${message}`,
+                '\n',
+            );
+        }
+    } catch {
+        // We don't show the message if stacks config is missing or incomplete
     }
 }
