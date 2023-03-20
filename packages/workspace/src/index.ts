6952fadff9d3ba664797ba2d59d797c81bc7414d
Nandor Szentpeteri <nandor.szentpeteri@amido.com>
2023-03-20 10:36:53 +0000

    fix(workspace): fix generator exports in index file

--- packages/workspace/src/index.ts
+++ packages/workspace/src/index.ts
@@ -1,1 +1,2 @@
-export { default as installGenerator } from './generators/init/generator';
+export { default as initGenerator } from './generators/init/generator';
+export { default as initDeploymentGenerator } from './generators/init-deployment/generator';
