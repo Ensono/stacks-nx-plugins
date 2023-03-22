c811fa1aadb8c8d116604e57a52e68bc8348fd7c
Saul France <saul.france@ensono.com>
2023-03-22 10:44:38 +0000

    refactor(azure-node): update project.json to set environment variable

--- packages/azure-node/src/generators/app-insights-deployment/generator.spec.ts
+++ packages/azure-node/src/generators/app-insights-deployment/generator.spec.ts
@@ -47,23 +47,32 @@ describe('app-insights-deployment generator', () => {
                 },
             },
         }));
+        updateJson(appTree, 'next-app/project.json', projectJson => ({
+            ...projectJson,
+            targets: {
+                'helm-upgrade': {
+                    executor: 'nx:run-commands',
+                    options: {
+                        commands: [
+                            {
+                                command:
+                                    'helm upgrade --create-namespace --install next-app next-app -n nx --atomic --wait',
+                                forwardAllArgs: false,
+                            },
+                        ],
+                        cwd: 'apps/next-app/deploy/helm/nonprod',
+                    },
+                },
+            },
+        }));
     });
 
-    it('should generate redis helm deploy values', async () => {
+    it('should update project.json with environment variable', async () => {
         await generator(appTree, options);
 
-        expect(
-            appTree.exists('next-app/deploy/helm/nonprod/appinsights.yaml'),
-        ).toBeTruthy();
-        expect(
-            appTree.exists('next-app/deploy/helm/prod/appinsights.yaml'),
-        ).toBeTruthy();
-
-        const helmRedisValues = appTree.read(
-            'next-app/deploy/helm/nonprod/appinsights.yaml',
-        );
-        expect(helmRedisValues.toString()).toContain(
-            'APPLICATIONINSIGHTS_CONNECTION_STRING',
+        const projectFile = appTree.read('next-app/project.json');
+        expect(projectFile.toString()).toContain(
+            'env.APPLICATIONINSIGHTS_CONNECTIONS_STRING=$APPLICATIONINSIGHTS_CONNECTIONS_STRING',
         );
     });
 });
