bbc72d7157fe418f78feba6400972eaa6bce2759
Saul France <saul.france@ensono.com>
2023-03-23 09:59:49 +0000

    fix(next): update redis connection string output

--- packages/next/src/generators/next-auth-redis-deployment/files/azure/deploy/terraform/redis.tf
+++ packages/next/src/generators/next-auth-redis-deployment/files/azure/deploy/terraform/redis.tf
@@ -52,5 +52,5 @@ resource "azurerm_redis_cache" "default_primary" {
 ###
 output "redis_connection_string" {
   sensitive = true
-  value     = "redis://${azurerm_redis_cache.default_primary.primary_access_key}@${azurerm_redis_cache.default_primary.hostname}:${azurerm_redis_cache.default_primary.ssl_port}"
+  value     = "rediss://:${azurerm_redis_cache.default_primary.primary_access_key}@${azurerm_redis_cache.default_primary.hostname}:${azurerm_redis_cache.default_primary.ssl_port}"
 }
