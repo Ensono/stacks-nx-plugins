7169ff65d4e6f4eb3299879701ba4d6449a9a4cc
Nandor Szentpeteri <nandor.szentpeteri@amido.com>
2023-03-28 13:18:43 +0100

    feat(common-test): add unit tests

---
+++ packages/common/test/src/lib/stacks-attributes.spec.ts
@@ -0,0 +1,62 @@
+import { Tree } from '@nrwl/devkit';
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+
+import { addStacksAttributes } from '.';
+
+describe('stacks', () => {
+    let tree: Tree;
+
+    beforeEach(async () => {
+        tree = createTreeWithEmptyWorkspace();
+    });
+
+    describe('addStacksAttributes', () => {
+        it('should add stacks attributes to nx.json and return with the values', () => {
+            const expectedStacksConfig = {
+                business: {
+                    company: 'Amido',
+                    domain: 'stacks',
+                    component: 'nx',
+                },
+                domain: {
+                    internal: 'test.com',
+                    external: 'test.dev',
+                },
+                cloud: {
+                    region: 'euw',
+                    platform: 'azure',
+                },
+                pipeline: 'azdo',
+                terraform: {
+                    group: 'terraform-group',
+                    storage: 'terraform-storage',
+                    container: 'terraform-container',
+                },
+                vcs: {
+                    type: 'github',
+                    url: 'remote.git',
+                },
+            };
+            const expectedStacksExecutedGenerators = {
+                project: {
+                    'test-project': [],
+                },
+                workspace: [],
+            };
+
+            const result = addStacksAttributes(tree, 'test-project');
+
+            const nxJson = JSON.parse(tree.read('nx.json', 'utf-8') as string);
+            expect(nxJson.stacks).toBeTruthy();
+            expect(nxJson.stacks.config).toMatchObject(expectedStacksConfig);
+            expect(nxJson.stacks.executedGenerators).toMatchObject(
+                expectedStacksExecutedGenerators,
+            );
+
+            expect(result.stacksConfig).toMatchObject(expectedStacksConfig);
+            expect(result.stacksExecutedGenerators).toMatchObject(
+                expectedStacksExecutedGenerators,
+            );
+        });
+    });
+});
