fc5aaa3b0a3b5b29b4b2d3003b08e6f2339c825c
Metecan Aydin <metecan.aydin@amido.com>
2023-03-30 10:35:54 +0100

    refactor(next): enable notify message for user post generator re-run

--- packages/next/src/generators/init-deployment/generator.ts
+++ packages/next/src/generators/init-deployment/generator.ts
@@ -24,6 +24,7 @@ export default async function initDeploymentGenerator(
             tree,
             options.project,
             'NextInitDeployment',
+            true,
         )
     )
         return false;
