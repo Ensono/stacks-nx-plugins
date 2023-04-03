9a2d9a59c107f2555f55b190356474cf26a44e31
Metecan Aydin <metecan.aydin@amido.com>
2023-04-03 10:14:53 +0100

    fix(workspace): update lint-staged.config file with new file structure

--- packages/workspace/src/generators/init/files/lintstaged/lint-staged.config.js
+++ packages/workspace/src/generators/init/files/lintstaged/lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     '*': ['nx affected --target=lint --uncommitted'],
-    '**/build/helm/**/*': ['nx affected --target=helm-lint --uncommitted'],
+    '**/libs/next-helm-chart/**/*': ['nx affected --target=lint --uncommitted'],
     '*.tf|*.tfvars|.terraform.lock.hcl': [
         'nx affected --target=terraform-fmt --uncommitted',
     ],
