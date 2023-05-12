70b0ccd63dfb1a251e3d3bce64c9a69b6c9b9d00
Daniel Phillips <daniel.phillips@ensono.com>
2023-05-12 14:31:48 +0100

    chore: [nx migration] update-configs-jest-29

--- packages/azure-react/jest.config.ts
+++ packages/azure-react/jest.config.ts
@@ -1,13 +1,14 @@
 export default {
     displayName: 'azure-react',
     preset: '../../jest.preset.js',
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.spec.json',
-        },
-    },
+    globals: {},
     transform: {
-        '^.+\\.[tj]s$': 'ts-jest',
+        '^.+\\.[tj]s$': [
+            'ts-jest',
+            {
+                tsconfig: '<rootDir>/tsconfig.spec.json',
+            },
+        ],
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
     coverageDirectory: '../../coverage/packages/azure-react',
