72d5a39790b5e34d361070711f804cac806febed
Daniel Phillips <daniel.phillips@ensono.com>
2023-05-12 16:18:20 +0100

    Update cypress versions.ts

--- packages/cypress/src/versions.ts
+++ packages/cypress/src/versions.ts
@@ -1,5 +1,5 @@
 export const CYPRESS = '^12.12.0';
-export const NRWLCYPRESS = '15.9.3';
+export const NRWLCYPRESS = '15.9.4';
 export const CYPRESSMULTIREPORTERS = '^1.6.3';
 export const MOCHAWESOME = '^7.1.3';
 export const MOCHAWESOMEMERGE = '^4.3.0';
