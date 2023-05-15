0b114ae4fcb40f0cd6829402fb85e02cadb06efb
Metecan Aydin <metecan.aydin@amido.com>
2023-05-15 14:54:46 +0100

    feat(rest-client): add generator to create orval config file inside new nx library

--- packages/rest-client/src/generators/openapi-client/generator.ts
+++ packages/rest-client/src/generators/openapi-client/generator.ts
@@ -1,25 +1,49 @@
+import { copyFiles, normalizeOptions } from '@ensono-stacks/core';
 import {
-    NormalizedSchema as BaseNormalizedSchema,
-    normalizeOptions,
-} from '@ensono-stacks/core';
-import { formatFiles, getWorkspaceLayout, names, Tree } from '@nrwl/devkit';
+    addDependenciesToPackageJson,
+    formatFiles,
+    generateFiles,
+    getWorkspaceLayout,
+    Tree,
+} from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/js';
 import path from 'path';
 
+import { orvalVersion } from '../../../utils/versions';
 import { OpenapiClientGeneratorSchema } from './schema';
-
-type NormalizedSchema = BaseNormalizedSchema<OpenapiClientGeneratorSchema>;
 
 export default async function generate(
     tree: Tree,
     options: OpenapiClientGeneratorSchema,
 ) {
+    if (!tree.exists(options.schema)) {
+        throw new Error(
+            'Provided schema does not exist in the workspace. Please check and try again.',
+        );
+    }
+
     const normalizedOptions = normalizeOptions(tree, options);
 
     // Use the existing library generator
     await libraryGenerator(tree, normalizedOptions);
+
     // Delete the default generated lib folder
     tree.delete(path.join(normalizedOptions.projectRoot, 'src', 'lib'));
 
+    const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${options.name}`;
+
+    const schemaPath = path.basename(options.schema);
+
+    // Copy schema into generated lib folder
+    copyFiles(tree, `./${options.schema}`, `./${projectRoot}/${schemaPath}`);
+
+    // Generate orval.config.js in new lib folder
+    generateFiles(tree, path.join(__dirname, 'files'), projectRoot, {
+        schemaName: options.name,
+        schemaPath,
+    });
+
     await formatFiles(tree);
+
+    return addDependenciesToPackageJson(tree, {}, { orval: orvalVersion });
 }
--- packages/rest-client/utils/versions.ts
+++ packages/rest-client/utils/versions.ts
@@ -1,1 +1,2 @@
 export const axiosVersion = '1.3.4';
+export const orvalVersion = '6.15.0';
