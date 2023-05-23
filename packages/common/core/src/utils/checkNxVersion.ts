792a695c1fc741eaebf4cb5ae387ad1b0b237419
Metecan Aydin <metecan.aydin@amido.com>
2023-05-23 16:10:30 +0100

    fix(root): set nx nrwl packages to 15.9.4 and scope to 15.x

--- packages/common/core/src/utils/checkNxVersion.ts
+++ packages/common/core/src/utils/checkNxVersion.ts
@@ -1,12 +1,12 @@
 import chalk from 'chalk';
 import semver from 'semver';
 
-import { NX_VERSION } from './versions';
+import { NX_VERSION, NX_VERSION_SCOPE } from './versions';
 
 export async function checkNxVersion(nxVersion: string) {
     const setNxVersion = nxVersion === 'latest' ? NX_VERSION : nxVersion;
 
-    const matchVersion = semver.satisfies(setNxVersion, NX_VERSION);
+    const matchVersion = semver.satisfies(setNxVersion, NX_VERSION_SCOPE);
 
     if (!matchVersion) {
         console.error(
