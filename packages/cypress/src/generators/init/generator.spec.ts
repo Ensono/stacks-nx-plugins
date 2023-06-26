fd6edfb786220bbdad748f4e83c1df93e37e0b6a
Metecan Aydin <metecan.aydin@amido.com>
2023-06-26 10:04:40 +0100

    fix(cypress): fix unit test for nx package version

--- packages/cypress/src/generators/init/generator.spec.ts
+++ packages/cypress/src/generators/init/generator.spec.ts
@@ -108,7 +108,7 @@ describe('should run successfully with default options', () => {
         const packageJson = readJson(appTree, 'package.json');
         expect(packageJson?.devDependencies).toMatchObject({
             cypress: CYPRESS_VERSION,
-            '@nx/cypress': NXCYPRESS_VERSION,
+            '@nx/cypress': '16.3.2',
             'cypress-multi-reporters': CYPRESSMULTIREPORTERS_VERSION,
             mochawesome: MOCHAWESOME_VERSION,
             'mochawesome-merge': MOCHAWESOMEMERGE_VERSION,
