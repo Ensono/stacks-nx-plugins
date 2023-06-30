a4a1e5392f03cb63d2e8668c6cb28d94e33a95bb
Metecan Aydin <metecan.aydin@amido.com>
2023-06-30 12:52:43 +0100

    test(create): set preset to next in e2e tests

--- e2e/create-e2e/tests/create.spec.ts
+++ e2e/create-e2e/tests/create.spec.ts
@@ -210,7 +210,7 @@ describe('create', () => {
     it('can install with cypress set as e2eTestRunner', async () => {
         const run = () =>
             execSync(
-                'npx --yes @ensono-stacks/create-stacks-workspace@latest proj --business.company=Ensono --business.domain=Stacks --business.component=Nx --cloud.platform=azure --cloud.region=euw --domain.internal=nonprod.amidostacks.com --domain.external=prod.amidostacks.com --terraform.group=tf-group --terraform.storage=tf-storage --terraform.container=tf-container --vcs.type=github --preset=apps --appName=test-app --e2eTestRunner=cypress --no-nxCloud --skipGit --no-interactive --verbose',
+                'npx --yes @ensono-stacks/create-stacks-workspace@latest proj --business.company=Ensono --business.domain=Stacks --business.component=Nx --cloud.platform=azure --cloud.region=euw --domain.internal=nonprod.amidostacks.com --domain.external=prod.amidostacks.com --terraform.group=tf-group --terraform.storage=tf-storage --terraform.container=tf-container --vcs.type=github --preset=next --appName=test-app --e2eTestRunner=cypress --no-nxCloud --skipGit --no-interactive --verbose',
                 {
                     cwd: temporaryDirectory,
                     stdio: 'inherit',
