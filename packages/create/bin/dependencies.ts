61db41e2399360b0b64d17d08f14657b1f97c85d
Metecan Aydin <metecan.aydin@amido.com>
2023-06-30 10:36:21 +0100

    fix(create): ensure is cypress is also selected we set to none

--- packages/create/bin/dependencies.ts
+++ packages/create/bin/dependencies.ts
@@ -26,7 +26,8 @@ export function normaliseForwardedArgv(
 
     // As playwright is not supported by the Nx generator for e2eTestRunner, we pass none to proceed and later install it ourselves as part of Stacks Playwright plugin.
     updatedForwardArgv['e2eTestRunner'] =
-        updatedForwardArgv['e2eTestRunner'] === 'playwright'
+        updatedForwardArgv['e2eTestRunner'] === 'playwright' ||
+        updatedForwardArgv['e2eTestRunner'] === 'cypress'
             ? 'none'
             : updatedForwardArgv['e2eTestRunner'];
     return updatedForwardArgv;
