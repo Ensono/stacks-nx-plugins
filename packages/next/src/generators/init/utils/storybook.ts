a81cdbf1248c1e1342c26f2dd3a1de2c4b10ba39
Elliot Evans <ell15evans.nuls@googlemail.com>
2023-10-02 23:01:27 +0100

    test(root): added more tests to storybook generator

--- packages/next/src/generators/init/utils/storybook.ts
+++ packages/next/src/generators/init/utils/storybook.ts
@@ -4,52 +4,93 @@ import {
     normalizeOptions,
 } from '@ensono-stacks/core';
 import {
+    addDependenciesToPackageJson,
     generateFiles,
     GeneratorCallback,
+    readJson,
     readProjectConfiguration,
+    readRootPackageJson,
     runTasksInSerial,
     Tree,
 } from '@nx/devkit';
+import chalk from 'chalk';
 import path from 'path';
 
+import {
+    ADDON_A11Y_STORYBOOK_VERSION,
+    ADDON_ACTIONS_STORYBOOK_VERSION,
+    ADDON_JEST_STORYBOOK_VERSION,
+    ADDON_LINKS_STORYBOOK_VERSION,
+    MANAGER_API_STORYBOOK_VERSION,
+    NEXTJS_STORYBOOK_VERSION,
+    NX_STORYBOOK_VERSION,
+    PREVIEW_API_STORYBOOK_VERSION,
+    THEMING_STORYBOOK_VERSION,
+} from './constants';
 import { NextGeneratorSchema } from '../schema';
+
+function addStorybookDependencies(tree: Tree) {
+    return addDependenciesToPackageJson(
+        tree,
+        {},
+        {
+            '@nx/storybook': NX_STORYBOOK_VERSION,
+            '@storybook/nextjs': NEXTJS_STORYBOOK_VERSION,
+            '@storybook/addon-links': ADDON_LINKS_STORYBOOK_VERSION,
+            '@storybook/manager-api': MANAGER_API_STORYBOOK_VERSION,
+            '@storybook/preview-api': PREVIEW_API_STORYBOOK_VERSION,
+            '@storybook/addon-a11y': ADDON_A11Y_STORYBOOK_VERSION,
+            '@storybook/addon-actions': ADDON_ACTIONS_STORYBOOK_VERSION,
+            '@storybook/addon-jest': ADDON_JEST_STORYBOOK_VERSION,
+            '@storybook/theming': THEMING_STORYBOOK_VERSION,
+        },
+    );
+}
 
 export function addStorybook(tree: Tree, options: NextGeneratorSchema) {
     const normalizedOptions = normalizeOptions(tree, {
         ...options,
         name: options.project,
     });
+
     const project = readProjectConfiguration(
         tree,
         normalizedOptions.projectName,
     );
 
-    const callbackTasks: GeneratorCallback[] = [];
+    try {
+        addStorybookDependencies(tree);
 
-    callbackTasks.push(
-        () =>
-            execAsync(
-                'npm install -D @nx/storybook',
-                project.root,
-            ) as Promise<void>,
-        () =>
-            execAsync(
-                'npm install -D @storybook/nextjs @storybook/addon-links @storybook/manager-api @storybook/preview-api @storybook/addon-a11y @storybook/addon-actions @storybook/addon-jest @storybook/theming',
-                project.root,
-            ) as Promise<void>,
-        () =>
-            execAsync(
-                'nx g @nx/react:storybook-configuration ui',
-                project.root,
-            ) as Promise<void>,
-    );
+        // TODO: fix npm command issue
+        // execAsync(
+        //     `npx nx g @nx/react:storybook-configuration ${options.project} --configureCypress=false --generateCypressSpecs=false --no-interactive`,
+        //     project.root,
+        // );
 
-    generateFiles(
-        tree,
-        path.join(__dirname, `../files/.storybook`),
-        project.root,
-        {},
-    );
+        generateFiles(tree, path.join(__dirname, `../files`), project.root, {});
 
-    return runTasksInSerial(...callbackTasks);
+        return () => {};
+    } catch {
+        console.error(
+            chalk.red`Failed to install NX React Storybook configuration`,
+        );
+
+        return () => {};
+    }
+
+    // return runTasksInSerial(
+    //     addStorybookDependencies(tree),
+    //     () =>
+    //         execAsync(
+    //             `nx g @nx/react:storybook-configuration ${options.project} --no-interactive`,
+    //             project.root,
+    //         ) as Promise<void>,
+    //     () =>
+    //         generateFiles(
+    //             tree,
+    //             path.join(__dirname, `../files`),
+    //             project.root,
+    //             {},
+    //         ),
+    // );
 }
