1a9559d7ad141d3a9b6f3d481ab2ddab5e743ea0
Chris Winch <chris.winch@ensono.com>
2024-05-09 12:26:13 +0100

    test(root): add testEnvironment config to all packages

--- packages/create/jest.config.ts
+++ packages/create/jest.config.ts
@@ -2,6 +2,7 @@ export default {
     displayName: 'create',
     preset: '../../jest.preset.js',
     globals: {},
+    testEnvironment: 'node',
     transform: {
         '^.+\\.[tj]s$': [
             'ts-jest',
