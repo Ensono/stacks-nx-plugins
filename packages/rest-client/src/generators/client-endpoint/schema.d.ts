bc14d500bc0b758e834e8101a3f673a16d63e847
Gareth Cozens <gareth.cozens@amido.com>
2024-06-25 09:07:11 +0100

    fix(rest-client): update client generator for nx upgrade

--- packages/rest-client/src/generators/client-endpoint/schema.d.ts
+++ packages/rest-client/src/generators/client-endpoint/schema.d.ts
@@ -5,5 +5,7 @@ export interface ClientEndpointGeneratorSchema {
     endpointVersion: number;
     methods: string[];
     directory: string;
+    projectNameAndRootFormat: 'as-provided' | 'derived';
+    importPath?: string;
     tags?: string;
 }
