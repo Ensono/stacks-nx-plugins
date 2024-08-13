0e26ed86fc34804ccfefe3c2e40526d5f14ffd4c
Richard Slater <richard.slater@amido.com>
2024-08-13 12:58:14 +0100

    fix(rest-client): updated axios to 1.7.3
    Axios version embeeded in TypeScript file needed bumping

--- packages/rest-client/utils/versions.ts
+++ packages/rest-client/utils/versions.ts
@@ -1,4 +1,4 @@
-export const AXIOS_VERSION = '1.6.8';
+export const AXIOS_VERSION = '1.7.3';
 export const ORVAL_VERSION = '6.28.2';
 export const MSW_VERSION = '2.2.14';
 export const FAKERJS_VERSION = '8.4.1';
