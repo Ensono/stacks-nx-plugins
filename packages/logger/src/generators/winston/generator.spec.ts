9be5c4f57e1641d6ad2421a972b0b1281e1de946
Richard Slater <richard.slater@ensono.com>
2025-12-15 11:26:10 +0000

    deps(stacks-nx-plugins): more jest bumps

--- packages/logger/src/generators/winston/generator.spec.ts
+++ packages/logger/src/generators/winston/generator.spec.ts
@@ -61,7 +61,7 @@ describe('logger generator', () => {
             joinPathFragments(`custom/${appName}`, 'tsconfig.spec.json'),
             joinPathFragments(`custom/${appName}`, '.eslintrc.json'),
             joinPathFragments(`custom/${appName}`, 'package.json'),
-            joinPathFragments(`custom/${appName}`, 'jest.config.ts'),
+            joinPathFragments(`custom/${appName}`, 'jest.config.cts'),
             joinPathFragments(`custom/${appName}/src`, 'index.ts'),
             joinPathFragments(`custom/${appName}/src`, 'index.test.ts'),
             'jest.config.ts',
